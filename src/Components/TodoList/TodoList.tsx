b49ec02cf21a6eee04f131708fb517ee27d9cb62
Vladislav <typlomi@gmail.com>
2022-12-01 23:20:30 +0600

    анимация формы

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -7,7 +7,7 @@ const TodoList = () => {
   return (
     <ul className="todo-list">
       {TodoListStore.todoList.map(todo =>
-        <li className="todo-list__item todo" key={todo.id}>
+        <li className="todo-list__item" key={todo.id}>
           <Todo 
           title={todo.title} 
           />
